4e739e6102c8c6a71b0dc2566dc50898cfc72e72
Ardit <ardit.vogli03@gmail.com>
2023-06-16 12:44:23 +0200

    fixes

--- src/components/Filtra/Filter.js
+++ src/components/Filtra/Filter.js
@@ -1,22 +1,26 @@
 import React from "react";
-import filterLogo from '../../assets/filter.png'
-import styles from './Filter.module.css'
+import filterLogo from "../../assets/filter.png";
+import styles from "./Filter.module.css";
 
 const Filter = () => {
-    return (
-        <div className={styles.filter}>
-            <img src={filterLogo} alt="filter" className={styles['filter-logo']} />
-            <p className={styles['filter-title']}>Filtra</p>
-            <p className={styles['paragraph-one']}>Tariffa oraria</p>
-            <input type="range" className={styles['range']} />
-            <p className={styles['paragraph-two']}>Luogo lezione</p>
-            <div className={styles.checkbox}>
-                <input type="checkbox" value="Casa Tutor" /> <span>Casa Tutor</span>
-                <br /><br /><input type="checkbox" value="Casa Tutor" /> <span>Casa Studente</span>
-                <br /><br /><input type="checkbox" value="Casa Tutor" checked /> <span>Online</span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.filter}>
+      <img src={filterLogo} alt="filter" className={styles["filter-logo"]} />
+      <p className={styles["filter-title"]}>Filtra</p>
+      <p className={styles["paragraph-one"]}>Tariffa oraria</p>
+      <input type="range" className={styles["range"]} />
+      <p className={styles["paragraph-two"]}>Luogo lezione</p>
+      <div className={styles.checkbox}>
+        <input type="checkbox" value="Casa Tutor" /> <span>Casa Tutor</span>
+        <br />
+        <br />
+        <input type="checkbox" value="Casa Tutor" /> <span>Casa Studente</span>
+        <br />
+        <br />
+        <input type="checkbox" value="Casa Tutor" checked /> <span>Online</span>
+      </div>
+    </div>
+  );
+};
 
 export default Filter;
